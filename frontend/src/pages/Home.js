ff84c6c4341c3ef037ad752185488f3362777566
DYLAN OTINA <dylan.otina@gmail.com>
2021-06-23 17:04:00 -0700

    Added image for home carousel + shopping icon

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,33 @@
 import React from 'react'
 import {Carousel} from 'react-bootstrap'
+import sneaker2 from '../assets/sneaker2.jpg'
+import sneaker3 from '../assets/sneaker3.jpg'
+import sneaker4 from '../assets/sneaker4.jpg'
+
 
 function Home() {
     return (
-        <div>
-            <h1>MockNike</h1>
-            <h3>Just do it!</h3>
+        <div style={{height: '100%'}}>
             <Carousel>
-                <Carousel.Item>
+                <Carousel.Item style={{height : '600px'}}>
                     <img
                     className="d-block w-100"
-                    src=""
+                    src={sneaker3}
                     alt="First Slide"
+                    style={{objectFit: 'scale-down'}}
+                   
                     />
                     <Carousel.Caption>
                     <h3>First slide label</h3>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item  style={{height : '600px'}}>
                     <img
                     className="d-block w-100"
-                    src=""
+                    src={sneaker4}
                     alt="Second slide"
+                    style={{objectFit: 'scale-down'}}
                     />
 
                     <Carousel.Caption>
@@ -30,11 +35,12 @@ function Home() {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item  style={{height : '600px'}}>
                     <img
                     className="d-block w-100"
-                    src=""
+                    src={sneaker2}
                     alt="Third slide"
+                    style={{objectFit: 'scale-down'}}
                     />
 
                     <Carousel.Caption>
@@ -43,6 +49,10 @@ function Home() {
                     </Carousel.Caption>
                 </Carousel.Item> 
             </Carousel>
+            <div style={{border: '1px solid black', marginTop:'2vh', marginLeft:'10vw',width: '80%',display : 'flex' , justifyContent:'center'}}>
+                Recently Added :
+            </div>
+
         </div>
     )
 }
